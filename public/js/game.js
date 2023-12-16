f3a19f3752e30530dca9789c0281af36c2679bcf
Akshatha <71076330+qptr@users.noreply.github.com>
2023-12-16 12:58:32 +0530

    Make dino jump on spacebar

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,11 @@
 document.addEventListener("keydown", jump);
 
-function jump() {
+function jump(event) {
+    var spacebar = 32;
+    if (event.keyCode != spacebar) return;
     const dinosaur = document.getElementById("dinosaur");
+    dinosaur.style.gridRow = 1;
+    setTimeout(function () {
+        dinosaur.style.gridRow = 2;
+    }, 200)
 }
