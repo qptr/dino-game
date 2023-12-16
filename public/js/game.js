c22541ec29a85766d87cde8ba62d044dea120582
Akshatha <71076330+qptr@users.noreply.github.com>
2023-12-17 00:57:12 +0530

    Scroll cacti with random intervals between scrolls

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,6 @@
-document.getElementById("start-button").addEventListener("click", gameCourse);
+"use strict";
+
+document.getElementById("start-button").addEventListener("click", game);
 document.addEventListener("keydown", jump);
 
 function jump(event) {
@@ -11,23 +13,34 @@ function jump(event) {
     }, 400)
 }
 
-function gameCourse() {
+function game() {
     document.getElementById("start-button").disabled = true;
     const cactus = document.getElementById("cactus");
     const cactus2 = document.getElementById("cactus-2");
-    propelCactus(cactus);
+    scrollCactus(cactus);
     setTimeout(function () {
-        cactus2.style.visibility = "visible";
-        propelCactus(cactus2);
-    }, 800);
+        scrollCactus(cactus2);
+    }, randomDelay());
 }
 
-function propelCactus(cactus) {
-    setInterval(function () {
+function scrollCactus(cactus) {
+    cactus.style.visibility = "visible";
+    const position = setInterval(function () {
         if (window.getComputedStyle(cactus).gridColumn > 1) {
             cactus.style.gridColumn = window.getComputedStyle(cactus).gridColumn - 1;
         } else {
             cactus.style.gridColumn = 50;
+            cactus.style.visibility = "collapse";
+            clearInterval(position);
+            setTimeout(function () {
+                scrollCactus(cactus);
+            }, randomDelay());
         }
-    }, 100)
+    }, 100);
 }
+
+function randomDelay() {
+    const min = 300;
+    const max = 2000;
+    return Math.floor(Math.random() * (max - min) + min);
+  }
