c9264db00d1bb1ada4a9540045489725ed3655a0
Akshatha <71076330+qptr@users.noreply.github.com>
2023-12-16 16:28:15 +0530

    Move cactus on clicking start

--- public/js/game.js
+++ public/js/game.js
@@ -1,11 +1,24 @@
+document.getElementById("start-button").addEventListener("click", gameCourse);
 document.addEventListener("keydown", jump);
 
 function jump(event) {
-    var spacebar = 32;
+    const spacebar = 32;
     if (event.keyCode != spacebar) return;
     const dinosaur = document.getElementById("dinosaur");
     dinosaur.style.gridRow = 1;
     setTimeout(function () {
         dinosaur.style.gridRow = 2;
+    }, 300)
+}
+
+function gameCourse() {
+    const cactus = document.getElementById("cactus");
+    setInterval(function () {
+        if (window.getComputedStyle(cactus).gridColumn > 1) {
+            cactus.style.gridColumn = window.getComputedStyle(cactus).gridColumn - 1;
+        } else {
+            cactus.style.gridColumn = 50;
+        }
+        console.log(window.getComputedStyle(cactus).gridColumn)
     }, 200)
 }
